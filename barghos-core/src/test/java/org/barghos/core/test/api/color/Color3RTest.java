a99a93fd9b23d2a3b8b7d612ee659198c8a402b0
picatrix1899 <FlorianZilkenat@gmx.de>
2021-02-12 17:19:46 +0100

    Added unit tests for color api.

---
+++ barghos-core/src/test/java/org/barghos/core/test/api/color/Color3RTest.java
@@ -0,0 +1,170 @@
+package org.barghos.core.test.api.color;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import org.barghos.core.api.color.Color3R;
+
+/**
+ * This class provides component tests for the interface {@link Color3R}.
+ * 
+ * @author picatrix1899
+ * 
+ *  @since 1.0.0.0
+ */
+public class Color3RTest
+{
+	/**
+	 * This test ensures, that the function {@link Color3R#getX()} returns
+	 * the value from {@link Color3R#getUnityR()}.
+	 * 
+	 * @since 1.0.0.0
+	 */
+	@Test
+	void getXTest()
+	{
+		Color3R c = new Color3R() {
+			@Override
+			public float getUnityR()
+			{
+				return 2.1f;
+			}
+			
+			@Override
+			public float getUnityG()
+			{
+				return 3.2f;
+			}
+			
+			@Override
+			public float getUnityB()
+			{
+				return 4.3f;
+			}
+			
+			@Override
+			public int getR()
+			{
+				return 0;
+			}
+			
+			@Override
+			public int getG()
+			{
+				return 0;
+			}
+			
+			@Override
+			public int getB()
+			{
+				return 0;
+			}
+		};
+		
+		assertEquals(2.1f, c.getX());
+		assertEquals(c.getUnityR(), c.getX());
+	}
+	
+	/**
+	 * This test ensures, that the function {@link Color3R#getY()} returns
+	 * the value from {@link Color3R#getUnityG()}.
+	 * 
+	 * @since 1.0.0.0
+	 */
+	@Test
+	void getYTest()
+	{
+		Color3R c = new Color3R() {
+			@Override
+			public float getUnityR()
+			{
+				return 2.1f;
+			}
+			
+			@Override
+			public float getUnityG()
+			{
+				return 3.2f;
+			}
+			
+			@Override
+			public float getUnityB()
+			{
+				return 4.3f;
+			}
+			
+			@Override
+			public int getR()
+			{
+				return 0;
+			}
+			
+			@Override
+			public int getG()
+			{
+				return 0;
+			}
+			
+			@Override
+			public int getB()
+			{
+				return 0;
+			}
+		};
+		
+		assertEquals(3.2f, c.getY());
+		assertEquals(c.getUnityG(), c.getY());
+	}
+	
+	/**
+	 * This test ensures, that the function {@link Color3R#getZ()} returns
+	 * the value from {@link Color3R#getUnityB()}.
+	 * 
+	 * @since 1.0.0.0
+	 */
+	@Test
+	void getZTest()
+	{
+		Color3R c = new Color3R() {
+			@Override
+			public float getUnityR()
+			{
+				return 2.1f;
+			}
+			
+			@Override
+			public float getUnityG()
+			{
+				return 3.2f;
+			}
+			
+			@Override
+			public float getUnityB()
+			{
+				return 4.3f;
+			}
+			
+			@Override
+			public int getR()
+			{
+				return 0;
+			}
+			
+			@Override
+			public int getG()
+			{
+				return 0;
+			}
+			
+			@Override
+			public int getB()
+			{
+				return 0;
+			}
+		};
+		
+		assertEquals(4.3f, c.getZ());
+		assertEquals(c.getUnityB(), c.getZ());
+	}
+}
---
+++ barghos-core/src/test/java/org/barghos/core/test/api/color/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains component tests for the color system api.
+ * 
+ * @author picatrix1899
+ * 
+ * @since 1.0.0.0
+ */
+package org.barghos.core.test.api.color;
