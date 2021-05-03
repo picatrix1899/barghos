7f4629161f4eb2d26b56590c5368cc8fbb09040f
picatrix1899 <FlorianZilkenat@gmx.de>
2021-05-03 21:15:33 +0200

    Fixed some Javadoc issues.

--- barghos-core/src/main/java/org/barghos/core/api/tuple3/Tup3bigiR.java
+++ barghos-core/src/main/java/org/barghos/core/api/tuple3/Tup3bigiR.java
@@ -144,8 +144,8 @@ public interface Tup3bigiR
 	 * <p>
 	 * This can be used for type continuety.
 	 * This way even while only using abstractions it is possible to create
-	 * new instances of the original. It is similar to the {@link #clone()}
-	 * function but the {@link #clone()} function requires the returned instance to be
+	 * new instances of the original. It is similar to the {@link Object#clone()}
+	 * function but the {@link Object#clone()} function requires the returned instance to be
 	 * writable.
 	 * This function on the other hand allows for example the usage of factories.
 	 * 
