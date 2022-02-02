3240643aedbe6c537a673fb2f1c46cead5ac05a1
picatrix1899 <FlorianZilkenat@gmx.de>
2022-02-02 22:01:24 +0100

    Added the functional interface GenericFunction2.

---
+++ barghos-core/src/main/java/org/barghos/core/api/util/GenericFunction2.java
@@ -0,0 +1,27 @@
+package org.barghos.core.api.util;
+
+
+/**
+ * This functional interface represents a function with a generic return type and two generic parameters.
+ * 
+ * @param <A> The type of the first parameter.
+ * @param <B> The type of the second parameter.
+ * @param <T> The return type.
+ * 
+ * @author picatrix1899
+ * 
+ * @since 1.0.0.0
+ */
+@FunctionalInterface
+public interface GenericFunction2<A,B,T>
+{
+	/**
+	 * Invokes the function.
+	 * 
+	 * @param a The first parameter.
+	 * @param b The second parameter.
+	 * 
+	 * @return The result of the function.
+	 */
+	T invoke(A a, B b);
+}
