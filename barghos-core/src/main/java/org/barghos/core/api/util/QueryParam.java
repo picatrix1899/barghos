1f03df368f0a337317ad5489ccb019c21f80753a
picatrix1899 <FlorianZilkenat@gmx.de>
2022-04-15 16:06:13 +0200

    Added the annotation @QueryParam for documentation.

---
+++ barghos-core/src/main/java/org/barghos/core/api/util/QueryParam.java
@@ -0,0 +1,28 @@
+package org.barghos.core.api.util;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(SOURCE)
+@Target(PARAMETER)
+
+/**
+ * Indicates that the annotated parameter is used as a query parameter.
+ * Query parameters are used to shift the responsibility for creating and maintaining the
+ * instance of a return object to the function caller. The type of a query parameter
+ * always has to be mutable in some form. This could be for example through a "setter" function.
+ * As the caller has the resposibility for instance management it is possible to either reuse
+ * an existing instance (i.e. an array) or utilize an instance pool. With this it is possible to
+ * increase the performance by cutting out unnecessary allocations.
+ * 
+ * @author picatrix1899
+ */
+public @interface QueryParam
+{
+
+}
