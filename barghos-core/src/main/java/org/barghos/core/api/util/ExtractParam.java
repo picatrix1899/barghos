cc50f85862aa9465e205426e75590dac33b0dfc6
picatrix1899 <FlorianZilkenat@gmx.de>
2022-04-15 17:15:31 +0200

    Changed @QueryParam to @ExtractParam.

--- barghos-core/src/main/java/org/barghos/core/api/util/ExtractParam.java
+++ barghos-core/src/main/java/org/barghos/core/api/util/ExtractParam.java
@@ -9,20 +9,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * Indicates that the annotated parameter is used as a query parameter.
- * Query parameters are used to shift the responsibility for creating and maintaining the
- * instance of a return object to the function caller. The type of a query parameter
+ * Indicates that the annotated parameter is used as a extraction parameter.
+ * Extraction parameters are used to shift the responsibility for creating and maintaining the
+ * instance of a return object to the function caller. The type of a extraction parameter
  * always has to be mutable in some form. This could be for example through a "setter" function.
  * As the caller has the resposibility for instance management it is possible to either reuse
  * an existing instance (i.e. an array) or utilize an instance pool. With this it is possible to
- * increase the performance by cutting out unnecessary allocations. A query parameter must never be null.
+ * increase the performance by cutting out unnecessary allocations. A extraction parameter must never be null.
  * 
  * @author picatrix1899
  */
 @Documented
 @Retention(CLASS)
 @Target(PARAMETER)
-public @interface QueryParam
+public @interface ExtractParam
 {
 
 }
