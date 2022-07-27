7bceeec8188d38cc5362682c5005039cc9f8730f
picatrix1899 <FlorianZilkenat@gmx.de>
2022-07-27 18:53:59 +0200

    Added some Bufferable interfaces.

---
+++ barghos-core-api/src/main/java/org/barghos/core/api/util/buffer/BufferableDouble.java
@@ -0,0 +1,19 @@
+package org.barghos.core.api.util.buffer;
+
+import java.nio.DoubleBuffer;
+
+/**
+ * This interface provides functions for buffer interactions.
+ * 
+ * @author picatrix1899
+ */
+public interface BufferableDouble extends BufferableDoubleR
+{
+	/**
+	 * Puts data into the buffer.
+	 * @param buffer The buffer.
+	 * 
+	 * @return The buffer.
+	 */
+	BufferableDouble fromBuffer(DoubleBuffer buffer);
+}
