f6990467649f3d74f8d442312e72b9bad0e4ff8b
picatrix1899 <FlorianZilkenat@gmx.de>
2023-01-26 19:52:26 +0100

    Readded AABB2f.

--- barghos-math/src/main/java/org/barghos/math/bounds/AABB2f.java
+++ barghos-math/src/main/java/org/barghos/math/bounds/AABB2f.java
@@ -1,338 +1,315 @@
-//package org.barghos.math.bounds;
-//
-//import org.barghos.core.api.tuple2.Tup2fR;
-//import org.barghos.core.api.tuple2.Tup2fC;
-//import org.barghos.math.api.bounds.BoundingVolume2f;
-//import org.barghos.math.api.point.Point2fR;
-//import org.barghos.math.api.vector.Vec2fR;
-//import org.barghos.math.api.vector.Vec2fC;
-//import org.barghos.math.point.Point2f;
-//import org.barghos.math.vector.Vec2f;
-//
-//public class AABB2f implements BoundingVolume2f
-//{
-//	protected float minX;
-//	protected float minY;
-//	protected float maxX;
-//	protected float maxY;
-//	
-//	public AABB2f()
-//	{
-//		set(0, 0, 1, 1);
-//	}
-//	
-//	public AABB2f(AABB2f aabb)
-//	{
-//		set(aabb);
-//	}
-//	
-//	public AABB2f(Tup2fR min, Tup2fR max)
-//	{
-//		set(min, max);
-//	}
-//	
-//	public AABB2f(Tup2fR min, float maxX, float maxY)
-//	{
-//		set(min, maxX, maxY);
-//	}
-//	
-//	public AABB2f(float minX, float minY, Tup2fR max)
-//	{
-//		set(minX, minY, max);
-//	}
-//	
-//	public AABB2f(float minX, float minY, float maxX, float maxY)
-//	{
-//		set(minX, minY, maxX, maxY);
-//	}
-//	
-//	public AABB2f set(AABB2f aabb)
-//	{
-//		return set(aabb.getMinX(), aabb.getMinY(), aabb.getMaxX(), aabb.getMaxY());
-//	}
-//	
-//	public AABB2f set(Tup2fR min, Tup2fR max)
-//	{
-//		return set(min.getX(), min.getY(), max.getX(), max.getY());
-//	}
-//	
-//	public AABB2f set(Tup2fR min, float maxX, float maxY)
-//	{
-//		return set(min.getX(), min.getY(), maxX, maxY);
-//	}
-//	
-//	public AABB2f set(float minX, float minY, Tup2fR max)
-//	{
-//		return set(minX, minY, max.getX(), max.getY());
-//	}
-//	
-//	public AABB2f set(float minX, float minY, float maxX, float maxY)
-//	{
-//		this.minX = Math.min(minX, maxX);
-//		this.minY = Math.min(minY, maxY);
-//		this.maxX = Math.max(minX, maxX);
-//		this.maxY = Math.max(minY, maxY);
-//		
-//		return this;
-//	}
-//
-//	public Point2fR getMinAsPoint()
-//	{
-//		return new Point2f(this.minX, this.minY);
-//	}
-//	
-//	public Vec2fR getMinAsVector()
-//	{
-//		return new Vec2f(this.minX, this.minY);
-//	}
-//	
-//	public float getMinX()
-//	{
-//		return this.minX;
-//	}
-//	
-//	public float getMinY()
-//	{
-//		return this.minY;
-//	}
-//	
-//	public <T extends Tup2fC> T getMin(T res)
-//	{
-//		res.set(this.minX, this.minY);
-//		
-//		return res;
-//	}
-//	
-//	public Point2fR getMaxAsPoint()
-//	{
-//		return new Point2f(this.maxX, this.maxY);
-//	}
-//	
-//	public Vec2fR getMaxAsVector()
-//	{
-//		return new Vec2f(this.maxX, this.maxY);
-//	}
-//	
-//	public float getMaxX()
-//	{
-//		return this.maxX;
-//	}
-//	
-//	public float getMaxY()
-//	{
-//		return this.maxY;
-//	}
-//	
-//	public <T extends Tup2fC> T getMax(T res)
-//	{
-//		res.set(this.maxX, this.maxY);
-//		
-//		return res;
-//	}
-//	
-//	public Vec2fR getSize()
-//	{
-//		return getSize(new Vec2f());
-//	}
-//	
-//	public <T extends Vec2fC> T getSize(T res)
-//	{
-//		res.set(getSizeX(), getSizeY());
-//		
-//		return res;
-//	}
-//	
-//	public float getSizeX()
-//	{
-//		return this.maxX - this.minX;
-//	}
-//	
-//	public float getSizeY()
-//	{
-//		return this.maxY - this.minY;
-//	}
-//	
-//	public Point2fR getCenterAsPoint()
-//	{
-//		return getCenter(new Point2f());
-//	}
-//	
-//	public Vec2fR getCenterAsVector()
-//	{
-//		return getCenter(new Vec2f());
-//	}
-//	
-//	public <T extends Tup2fC> T getCenter(T res)
-//	{
-//		res.set(getCenterX(), getCenterY());
-//		
-//		return res;
-//	}
-//	
-//	public float getCenterX()
-//	{
-//		return (this.minX + this.maxX) * 0.5f;
-//	}
-//	
-//	public float getCenterY()
-//	{
-//		return (this.minY + this.maxY * 0.5f);
-//	}
-//	
-//	public AABB2f scale(Tup2fR t)
-//	{
-//		return scale(t.getX(), t.getY());
-//	}
-//	
-//	public AABB2f scale(float value)
-//	{
-//		return scale(value, value);
-//	}
-//	
-//	public AABB2f scale(float x, float y)
-//	{
-//		return scaleR(x, y, this);
-//	}
-//	
-//	public AABB2f scaleN(Tup2fR t)
-//	{
-//		return scaleN(t.getX(), t.getY());
-//	}
-//	
-//	public AABB2f scaleN(float value)
-//	{
-//		return scaleN(value, value);
-//	}
-//	
-//	public AABB2f scaleN(float x, float y)
-//	{
-//		return scaleR(x, y, new AABB2f());
-//	}
-//	
-//	public AABB2f scaleR(Tup2fR t, AABB2f res)
-//	{
-//		return scaleR(t.getX(), t.getY(), res);
-//	}
-//	
-//	public AABB2f scaleR(float value, AABB2f res)
-//	{
-//		return scaleR(value, value, res);
-//	}
-//	
-//	public AABB2f scaleR(float x, float y, AABB2f res)
-//	{
-//		res.set(this.minX * x,  this.minY * y, this.maxX * x, this.maxY * y);
-//		
-//		return res;
-//	}
-//	
-//	public AABB2f merge(Tup2fR min, Tup2fR max)
-//	{
-//		return merge(min.getX(), min.getY(), max.getX(), max.getY());
-//	}
-//	
-//	public AABB2f merge(Tup2fR min, float maxX, float maxY)
-//	{
-//		return merge(min.getX(), min.getY(), maxX, maxY);
-//	}
-//	
-//	public AABB2f merge(float minX, float minY, Tup2fR max)
-//	{
-//		return merge(minX, minY, max.getX(), max.getY());
-//	}
-//	
-//	public AABB2f merge(float minX, float minY, float maxX, float maxY)
-//	{
-//		return mergeR(minX, minY, maxX, maxY, this);
-//	}
-//	
-//	public AABB2f mergeN(Tup2fR min, Tup2fR max)
-//	{
-//		return mergeN(min.getX(), min.getY(), max.getX(), max.getY());
-//	}
-//	
-//	public AABB2f mergeN(Tup2fR min, float maxX, float maxY)
-//	{
-//		return mergeN(min.getX(), min.getY(), maxX, maxY);
-//	}
-//	
-//	public AABB2f mergeN(float minX, float minY, Tup2fR max)
-//	{
-//		return mergeN(minX, minY, max.getX(), max.getY());
-//	}
-//	
-//	public AABB2f mergeN(float minX, float minY, float maxX, float maxY)
-//	{
-//		return mergeR(minX, minY, maxX, maxY, new AABB2f());
-//	}
-//	
-//	public AABB2f mergeR(Tup2fR min, Tup2fR max, AABB2f res)
-//	{
-//		return mergeR(min.getX(), min.getY(), max.getX(), max.getY(), res);
-//	}
-//	
-//	public AABB2f mergeR(Tup2fR min, float maxX, float maxY, AABB2f res)
-//	{
-//		return mergeR(min.getX(), min.getY(), maxX, maxY, res);
-//	}
-//	
-//	public AABB2f mergeR(float minX, float minY, Tup2fR max, AABB2f res)
-//	{
-//		return mergeR(minX, minY, max.getX(), max.getY(), res);
-//	}
-//	
-//	public AABB2f mergeR(float minX, float minY, float maxX, float maxY, AABB2f res)
-//	{
-//		float newMinX = Math.min(this.minX, Math.min(minX, maxX));
-//		float newMinY = Math.min(this.minY, Math.min(minY, maxY));
-//		float newMaxX = Math.max(this.maxX, Math.max(minX, maxX));
-//		float newMaxY = Math.max(this.maxY, Math.max(minY, maxY));
-//		
-//		res.set(newMinX, newMinY, newMaxX, newMaxY);
-//		
-//		return res;
-//	}
-//	
-//	public boolean isInside(Tup2fR t)
-//	{
-//		return isInside(t.getX(), t.getY());
-//	}
-//	
-//	public boolean isInside(float x, float y)
-//	{
-//		if(this.maxX < x || x < this.minX) return false;
-//		if(this.maxY < y || y < this.minY) return false;
-//		
-//		return true;
-//	}
-//	
-//	public boolean isOnSurface(Tup2fR t)
-//	{
-//		return isOnSurface(t.getX(), t.getY());
-//	}
-//	
-//	public boolean isOnSurface(float x, float y)
-//	{
-//		if(maxX < x || x < minX) return false;
-//		if(maxY < y || y < minY) return false;
-//		
-//		if(x == minX || x == maxX) return true;
-//		if(y == minY || y == maxY) return true;
-//		
-//		return false;
-//	}
-//	
-//	/** {@inheritDoc}} */
-//	@Override
-//	public String toString()
-//	{
-//		return "aabb2f(min=(" + this.minX + ", " + this.minY + "), max=(" + this.maxX + ", " + this.maxY +  "))";
-//	}
-//	
-//	/** {@inheritDoc} */
-//	@Override
-//	public AABB2f clone()
-//	{
-//		return new AABB2f(this);
-//	}
-//}
+package org.barghos.math.bounds;
+
+import org.barghos.math.api.vector.Vec2fR;
+import org.barghos.math.api.vector.Vec2fUtils;
+import org.barghos.math.api.vector.SimpleVec2fC;
+import org.barghos.math.api.vector.SimpleVec2fR;
+import org.barghos.math.api.vector.Vec2fC;
+import org.barghos.math.vector.Vec2f;
+
+public class AABB2f
+{
+	protected float[] min = new float[2];
+	protected float[] max = new float[2];
+	
+	public AABB2f()
+	{
+		set(0, 0, 1, 1);
+	}
+	
+	public AABB2f(AABB2f aabb)
+	{
+		set(aabb);
+	}
+	
+	public AABB2f(SimpleVec2fR min, SimpleVec2fR max)
+	{
+		set(min, max);
+	}
+	
+	public AABB2f(SimpleVec2fR min, float maxX, float maxY)
+	{
+		set(min, maxX, maxY);
+	}
+	
+	public AABB2f(float minX, float minY, SimpleVec2fR max)
+	{
+		set(minX, minY, max);
+	}
+	
+	public AABB2f(float minX, float minY, float maxX, float maxY)
+	{
+		set(minX, minY, maxX, maxY);
+	}
+	
+	public AABB2f set(AABB2f aabb)
+	{
+		return set(aabb.getMinX(), aabb.getMinY(), aabb.getMaxX(), aabb.getMaxY());
+	}
+	
+	public AABB2f set(SimpleVec2fR min, SimpleVec2fR max)
+	{
+		return set(min.getX(), min.getY(), max.getX(), max.getY());
+	}
+	
+	public AABB2f set(SimpleVec2fR min, float maxX, float maxY)
+	{
+		return set(min.getX(), min.getY(), maxX, maxY);
+	}
+	
+	public AABB2f set(float minX, float minY, SimpleVec2fR max)
+	{
+		return set(minX, minY, max.getX(), max.getY());
+	}
+	
+	public AABB2f set(float minX, float minY, float maxX, float maxY)
+	{
+		Vec2fUtils.minVector(minX, minY, maxX, maxY, this.min);
+		Vec2fUtils.maxVector(minX, minY, maxX, maxY, this.max);
+		
+		return this;
+	}
+	
+	public Vec2fR getMinAsVector()
+	{
+		return new Vec2f(this.min);
+	}
+	
+	public float getMinX()
+	{
+		return this.min[0];
+	}
+	
+	public float getMinY()
+	{
+		return this.min[1];
+	}
+	
+	public <T extends SimpleVec2fC> T getMin(T res)
+	{
+		res.setArray(this.min);
+		
+		return res;
+	}
+	
+	public Vec2fR getMaxAsVector()
+	{
+		return new Vec2f(this.max);
+	}
+	
+	public float getMaxX()
+	{
+		return this.max[0];
+	}
+	
+	public float getMaxY()
+	{
+		return this.max[1];
+	}
+	
+	public <T extends SimpleVec2fC> T getMax(T res)
+	{
+		res.setArray(this.max);
+		
+		return res;
+	}
+	
+	public Vec2fR getSize()
+	{
+		return getSize(new Vec2f());
+	}
+	
+	public <T extends Vec2fC> T getSize(T res)
+	{
+		res.set(getSizeX(), getSizeY());
+		
+		return res;
+	}
+	
+	public float getSizeX()
+	{
+		return this.max[0] - this.min[0];
+	}
+	
+	public float getSizeY()
+	{
+		return this.max[1] - this.min[1];
+	}
+	
+	public Vec2fR getCenterAsVector()
+	{
+		return getCenter(new Vec2f());
+	}
+	
+	public <T extends SimpleVec2fC> T getCenter(T res)
+	{
+		res.set(getCenterX(), getCenterY());
+		
+		return res;
+	}
+	
+	public float getCenterX()
+	{
+		return (this.min[0] + this.max[0]) * 0.5f;
+	}
+	
+	public float getCenterY()
+	{
+		return (this.min[0] + this.max[1] * 0.5f);
+	}
+	
+	public AABB2f scale(SimpleVec2fR t)
+	{
+		return scale(t.getX(), t.getY());
+	}
+	
+	public AABB2f scale(float value)
+	{
+		return scale(value, value);
+	}
+	
+	public AABB2f scale(float x, float y)
+	{
+		return scaleR(x, y, this);
+	}
+	
+	public AABB2f scaleN(SimpleVec2fR t)
+	{
+		return scaleN(t.getX(), t.getY());
+	}
+	
+	public AABB2f scaleN(float value)
+	{
+		return scaleN(value, value);
+	}
+	
+	public AABB2f scaleN(float x, float y)
+	{
+		return scaleR(x, y, new AABB2f());
+	}
+	
+	public AABB2f scaleR(SimpleVec2fR t, AABB2f res)
+	{
+		return scaleR(t.getX(), t.getY(), res);
+	}
+	
+	public AABB2f scaleR(float value, AABB2f res)
+	{
+		return scaleR(value, value, res);
+	}
+	
+	public AABB2f scaleR(float x, float y, AABB2f res)
+	{
+		res.set(this.min[0] * x,  this.min[1] * y, this.max[0] * x, this.max[1] * y);
+		
+		return res;
+	}
+	
+	public AABB2f merge(SimpleVec2fR min, SimpleVec2fR max)
+	{
+		return merge(min.getX(), min.getY(), max.getX(), max.getY());
+	}
+	
+	public AABB2f merge(SimpleVec2fR min, float maxX, float maxY)
+	{
+		return merge(min.getX(), min.getY(), maxX, maxY);
+	}
+	
+	public AABB2f merge(float minX, float minY, SimpleVec2fR max)
+	{
+		return merge(minX, minY, max.getX(), max.getY());
+	}
+	
+	public AABB2f merge(float minX, float minY, float maxX, float maxY)
+	{
+		return mergeR(minX, minY, maxX, maxY, this);
+	}
+	
+	public AABB2f mergeN(SimpleVec2fR min, SimpleVec2fR max)
+	{
+		return mergeN(min.getX(), min.getY(), max.getX(), max.getY());
+	}
+	
+	public AABB2f mergeN(SimpleVec2fR min, float maxX, float maxY)
+	{
+		return mergeN(min.getX(), min.getY(), maxX, maxY);
+	}
+	
+	public AABB2f mergeN(float minX, float minY, SimpleVec2fR max)
+	{
+		return mergeN(minX, minY, max.getX(), max.getY());
+	}
+	
+	public AABB2f mergeN(float minX, float minY, float maxX, float maxY)
+	{
+		return mergeR(minX, minY, maxX, maxY, new AABB2f());
+	}
+	
+	public AABB2f mergeR(SimpleVec2fR min, SimpleVec2fR max, AABB2f res)
+	{
+		return mergeR(min.getX(), min.getY(), max.getX(), max.getY(), res);
+	}
+	
+	public AABB2f mergeR(SimpleVec2fR min, float maxX, float maxY, AABB2f res)
+	{
+		return mergeR(min.getX(), min.getY(), maxX, maxY, res);
+	}
+	
+	public AABB2f mergeR(float minX, float minY, SimpleVec2fR max, AABB2f res)
+	{
+		return mergeR(minX, minY, max.getX(), max.getY(), res);
+	}
+	
+	public AABB2f mergeR(float minX, float minY, float maxX, float maxY, AABB2f res)
+	{
+		float[] newMin = Vec2fUtils.minVector(this.min, Math.min(minX, maxX),  Math.min(minY, maxY), new float[2]);
+		float[] newMax = Vec2fUtils.maxVector(this.min, Math.max(minX, maxX),  Math.max(minY, maxY), new float[2]);
+		
+		res.set(newMin[0], newMin[1], newMax[0], newMax[1]);
+		
+		return res;
+	}
+	
+	public boolean isInside(SimpleVec2fR t)
+	{
+		return isInside(t.getX(), t.getY());
+	}
+	
+	public boolean isInside(float x, float y)
+	{
+		if(this.max[0] < x || x < this.min[0]) return false;
+		if(this.max[1] < y || y < this.min[1]) return false;
+		
+		return true;
+	}
+	
+	public boolean isOnSurface(SimpleVec2fR t)
+	{
+		return isOnSurface(t.getX(), t.getY());
+	}
+	
+	public boolean isOnSurface(float x, float y)
+	{
+		if(max[0] < x || x < min[0]) return false;
+		if(max[1] < y || y < min[1]) return false;
+		
+		if(x == min[0] || x == max[0]) return true;
+		if(y == min[1] || y == max[1]) return true;
+		
+		return false;
+	}
+	
+	/** {@inheritDoc}} */
+	@Override
+	public String toString()
+	{
+		return "aabb2f(min=(" + this.min[0] + ", " + this.min[1] + "), max=(" + this.max[0] + ", " + this.max[1] +  "))";
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public AABB2f clone()
+	{
+		return new AABB2f(this);
+	}
+}
